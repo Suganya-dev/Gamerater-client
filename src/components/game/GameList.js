16d03ce552d5509eb25f401afc467c854ef3e634
Suganya <suganprakash0621@gmail.com>
2021-02-16 21:42:12 -0600

    copy coded

---
+++ src/components/game/GameList.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<button className="btn btn-2 btn-sep icon-create"
+    onClick={() => {
+        history.push({ pathname: "/games/new" })
+    }}
+>Register New Game</button>
---
+++ src/components/game/Gameprovider.js
@@ -0,0 +1,14 @@
+
+
+const createGame = (game) => {
+    return fetch("", { })
+        .then()
+        .then()
+}
+
+const getGameTypes = () => {
+    return fetch("", { })
+        .then()
+        .then()
+}
+
